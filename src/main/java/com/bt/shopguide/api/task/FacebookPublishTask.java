cdaca1fd690b2e6d6a7b4f5b2421e21e3ec20b0f
caiting <68755498@qq.com>
2017-11-03 15:45:05 +0800

    1

--- src/main/java/com/bt/shopguide/api/task/FacebookPublishTask.java
+++ src/main/java/com/bt/shopguide/api/task/FacebookPublishTask.java
@@ -4,7 +4,13 @@ import com.bt.shopguide.api.util.HttpClientHelper;
 import com.google.gson.Gson;
 import org.apache.commons.lang.StringUtils;
 import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicHeader;
+import org.apache.http.util.EntityUtils;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
@@ -24,10 +30,26 @@ public class FacebookPublishTask {
     }
 
     public static void main(String[] args) throws Exception {
+        String json = "{\"longUrl\": \"http://www.dealswill.com\"}";
+        String apiUrl = "https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyDRM32LWTZK_49u6LmUa3le95CUfQ7eBRg";
         HttpClientHelper http = HttpClientHelper.getInstance();
-        Map m = new HashMap();
-        m.put("longUrl","http://www.dealswill.com/detail/19021");
-        System.out.println(http.doPost("https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyDRM32LWTZK_49u6LmUa3le95CUfQ7eBRg", m, new BasicHeader("Content-Type","application/json")));
+        CloseableHttpClient httpClient = http.getConnection();
+
+
+        HttpPost postRequest = new HttpPost(apiUrl);
+        postRequest.setHeader("Content-Type", "application/json");
+        postRequest.setEntity(new StringEntity(json, "UTF-8"));
+
+        HttpResponse response = httpClient.execute(postRequest);
+        String responseText = EntityUtils.toString(response.getEntity());
+
+        Gson gson = new Gson();
+        @SuppressWarnings("unchecked")
+        HashMap<String, String> res = gson.fromJson(responseText, HashMap.class);
+
+
+
+        System.out.println(res.get("id"));
 
     }
 }
